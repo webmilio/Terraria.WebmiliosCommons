468f390e57c1154096c7ce449146063648069c0f
webmilio <therealwebmilio@gmail.com>
2019-11-01 18:41:01 -0400

    Added extensions for finding which mod called what.

---
+++ Extensions/StackTraceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebmilioCommons.Extensions
+{
+    public static class StackTraceExtensions
+    {
+        public static Assembly GetFirstDifferentAssembly(this StackTrace stackTrace)
+        {
+            StackFrame[] frames = stackTrace.GetFrames();
+            Assembly thisCaller = frames[1].GetMethod().DeclaringType.Assembly;
+
+            for (int i = 1; i < frames.Length; i++)
+                if (frames[i].GetMethod().DeclaringType.Assembly != thisCaller)
+                    return frames[i].GetMethod().DeclaringType.Assembly;
+
+            return null;
+        }
+    }
+}
