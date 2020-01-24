4b787c987d55fc84c1963d4c21580ff242dfb8ef
webmilio <therealwebmilio@gmail.com>
2020-01-24 01:32:11 -0500

    no message

--- Extensions/EntityExtensions.cs
+++ Extensions/EntityExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.ID;
 using WebmilioCommons.Tinq;
 
 namespace WebmilioCommons.Extensions
@@ -46,6 +47,31 @@ namespace WebmilioCommons.Extensions
                 return MathHelper.ToDegrees(rotation);
 
             return rotation;
+        }
+
+        /// <summary></summary>
+        /// <param name="entity"></param>
+        /// <param name="requireTileUnder"></param>
+        /// <param name="feetXRadius"></param>
+        /// <returns></returns>
+        public static bool IsImmobile(this Entity entity, bool requireTileUnder = true, int feetXRadius = 0)
+        {
+            Vector2 velocity = entity.velocity;
+
+            if (velocity.X != 0 && velocity.Y != 0)
+                return false;
+
+            Vector2 center = entity.Center;
+            Point tileUnder = new Point((int) center.X, (int) (center.Y - entity.height / 2));
+
+            if (feetXRadius == 0)
+            {
+                if (Main.tile[tileUnder.X, tileUnder.Y].type == 0)
+                    return false;
+            }
+
+
+            return true;
         }
     }
 }
