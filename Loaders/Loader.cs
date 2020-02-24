c0191e4c77d1d3cd2cc952e65f0d9d25e5fb2703
webmilio <therealwebmilio@gmail.com>
2020-02-24 11:45:55 -0500

    Documentation for Loader.

--- Loaders/Loader.cs
+++ Loaders/Loader.cs
@@ -99,10 +99,20 @@ namespace WebmilioCommons.Loaders
             PostUnload();
         }
 
+        /// <summary>Called at the very beginning of the <see cref="Unload"/> method.</summary>
         protected virtual void PreUnload() { }
+
+        /// <summary>Called at the very end of the <see cref="Unload"/> method.</summary>
         protected virtual void PostUnload() { }
 
 
+        /// <summary>
+        /// Add an generic instance to the loaded items.
+        /// If <typeparamref name="T"/> implements <see cref="IHasUnlocalizedName"/>, it will also register it using <see cref="IHasUnlocalizedName.UnlocalizedName"/>.
+        /// </summary>
+        /// <param name="mod">The <see cref="Mod"/> from which the item comes.</param>
+        /// <param name="item">A generic instance.</param>
+        /// <returns></returns>
         protected T Add(Mod mod, T item)
         {
             int itemId = NextIndex;
@@ -133,10 +143,21 @@ namespace WebmilioCommons.Loaders
         [Obsolete("Use PostAdd(Mod mod, T item, Type type).")]
         protected virtual void PostAdd(Mod mod, T item) { }
 
+        /// <summary>Called after each time a subclass is added to the generic instances database.</summary>
+        /// <param name="mod">The <see cref="Mod"/> from which the generic instance originates.</param>
+        /// <param name="item">A generic instance.</param>
+        /// <param name="type">The <see cref="Type"/> from which the generic instance was created.</param>
         protected virtual void PostAdd(Mod mod, T item, Type type) { }
 
 
+        /// <summary>Creates a new instance of the requested type.</summary>
+        /// <param name="id">The auto-assigned Id of the generic instance.</param>
+        /// <returns>The newly instantiated type.</returns>
         public T New(int id) => New(typeById[id]);
+
+        /// <summary>Creates a new instance of the requested type.</summary>
+        /// <typeparam name="TType">The type of the generic instance to create; must be a child of <typeparamref name="T"/> and a non-abstract <c>class</c>.</typeparam>
+        /// <returns>The newly instantiated object.</returns>
         public TType New<TType>() where TType : class, T => New(typeof(TType)) as TType;
 
         public T New(Type type)
@@ -149,6 +170,12 @@ namespace WebmilioCommons.Loaders
             return item;
         }
 
+        /// <summary>Creates a new instance of the requested type.</summary>
+        /// <param name="unlocalizedName">
+        /// The corresponding value for <see cref="IHasUnlocalizedName.UnlocalizedName"/> of the desired type.
+        /// <typeparamref name="T"/> must implement <see cref="IHasUnlocalizedName"/>.
+        /// </param>
+        /// <returns>The newly instantiated object if <see cref="TypeHasUnlocalizedName"/>; otherwise <c>default</c>.</returns>
         public T New(string unlocalizedName)
         {
             if (!TypeHasUnlocalizedName)
@@ -185,10 +212,27 @@ namespace WebmilioCommons.Loaders
         }
 
 
+        /// <summary>Gets the Id of the generic instance of the same type as the one provided.</summary>
+        /// <param name="item">The type of the generic instance.</param>
+        /// <returns>The Id of the generic instance.</returns>
         public int GetId(T item) => GetId(item.GetType());
+
+        /// <summary>Gets the Id of the generic instance of the same type as the one provided.</summary>
+        /// <param name="type">The type of the generic instance.</param>
+        /// <returns>The Id of the generic instance.</returns>
         public int GetId(Type type) => idByType[type];
+
+        /// <summary>Gets the Id of the generic instance of the same type as the one provided.</summary>
+        /// <typeparam name="TType">The type of the generic instance.</typeparam>
+        /// <returns>The Id of the generic instance.</returns>
         public int GetId<TType>() where TType : T => GetId(typeof(TType));
 
+        /// <summary>
+        /// Gets the Id of the generic instance with the provided unlocalized name.
+        /// <see cref="TypeHasUnlocalizedName"/> must be true.
+        /// </summary>
+        /// <param name="unlocalizedName">The unlocalized name of the generic instance.</param>
+        /// <returns>The Id of the generic instance if found; otherwise <c>default</c>.</returns>
         public int GetId(string unlocalizedName)
         {
             if (!TypeHasUnlocalizedName)
@@ -198,6 +242,9 @@ namespace WebmilioCommons.Loaders
         }
 
 
+        /// <summary>Searches for a generic instance that matches the conditions defined by the specified predicate.</summary>
+        /// <param name="condition">The <see cref="Predicate{T}"/> delegate that defines the conditions of the generic instance to search for.</param>
+        /// <returns>The generic instance that matches the conditions defined by the specified predicate, if found; otherwise, the default value for type T.</returns>
         public T FindGeneric(Predicate<T> condition)
         {
             foreach (KeyValuePair<Type, T> kvp in genericByType)
@@ -228,8 +275,10 @@ namespace WebmilioCommons.Loaders
         /// <summary>How many subtypes of <see cref="T"/> have been loaded.</summary>
         public int Count => idByType.Count;
 
+        /// <summary>The first auto-assigned Id given to the generic instances.</summary>
         public int FirstIndex => FIRST_INDEX;
 
+        /// <summary>The next auto-assigned Id for the next added generic instance.</summary>
         public int NextIndex { get; private set; } = FIRST_INDEX;
     }
 }
