1937dbff4186bcf9fbb88618d50b9b426e5a138d
webmilio <therealwebmilio@gmail.com>
2020-02-29 15:49:21 -0500

    NetworkPacketBehavior documentation.

--- Networking/NetworkPacketBehavior.cs
+++ Networking/NetworkPacketBehavior.cs
@@ -1,10 +1,18 @@
 ﻿namespace WebmilioCommons.Networking
 {
+    /// <summary>The different behaviors for network packets.</summary>
     public enum NetworkPacketBehavior
     {
+        /// <summary>Sends the packet directly to the specified client (toWho).</summary>
         SendToClient,
+
+        /// <summary>Sends to all clients using the server as a bounce, but without processing said packet on the server (automatically resent).</summary>
         SendToAllClients,
+
+        /// <summary>Sends the packet only to the server (not resent).</summary>
         SendToServer,
+
+        /// <summary>Sends to all clients using the server as a bounce (automatically resent).</summary>
         SendToAll
     }
 }
