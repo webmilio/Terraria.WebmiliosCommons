e29bd315cd622b92eb389c53336759e38358a39f
webmilio <therealwebmilio@gmail.com>
2020-10-05 15:42:51 -0400

    Added Test Shader "FullGlow", not sure if it works.

---
+++ Effects/Shaders/Entities/FullGlow/FullGlow.fx
@@ -0,0 +1,17 @@
+﻿sampler uImage0 : register(s0);
+
+
+float4 FullGlow(float2 coords : TEXCOORD0) : COLOR0
+{
+    float4 color = tex2D(uImage0, coords);
+	
+    return color;
+}
+
+technique Technique1
+{
+    pass FullGlow
+    {
+        PixelShader = compile ps_2_0 FullGlow();
+    }
+}
---
+++ Effects/Shaders/Entities/FullGlow/FullGlowEffect.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Graphics.Shaders;
+
+namespace WebmilioCommons.Effects.Shaders.Entities.FullGlow
+{
+    public class FullGlowEffect : EntityMiscShaderEffect
+    {
+        protected MiscShaderData shaderData;
+        protected readonly string miscDictionaryKey;
+
+
+        public FullGlowEffect()
+        {
+            miscDictionaryKey = $"{GetType().FullName}";
+
+            if (shaderData == default)
+            {
+                shaderData = new MiscShaderData(new Ref<Effect>(WebmilioCommonsMod.Instance.GetEffect("Effects/FullGlow/FullGlow")), "FullGlow");
+                GameShaders.Misc[miscDictionaryKey] = shaderData;
+            }
+
+            GameShaders.Misc[miscDictionaryKey] = shaderData;
+        }
+
+
+        public override void Apply(SpriteBatch spriteBatch, Entity entity)
+        {
+            BeginStandardSpriteBatch(spriteBatch, BlendState.NonPremultiplied);
+
+            shaderData.Apply();
+
+            RestoreSpriteBatch(spriteBatch);
+        }
+    }
+}
