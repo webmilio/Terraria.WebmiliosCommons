495104687b8612451d986d8a13fd4f6fe32b65bc
webmilio <webmilio@users.noreply.github.com>
2021-10-18 01:35:20 -0400

    Cleaned up Buff class.

--- Buffs/StandardBuff.cs
+++ Buffs/StandardBuff.cs
@@ -8,47 +8,15 @@ namespace WebmilioCommons.Buffs
 {
     public abstract class StandardBuff : ModBuff
     {
-        protected StandardBuff(string displayName, string description, bool hideTime = false, bool save = false, bool persistent = false, bool canBeCleared = true) :
-            this(
-                new Dictionary<GameCulture, string>()
-                {
-                    {WebmilioCommonsMod.EnglishCulture, displayName}
-                },
-                new Dictionary<GameCulture, string>()
-                {
-                    {WebmilioCommonsMod.EnglishCulture, description}
-                },
-                hideTime, save, persistent, canBeCleared)
+        protected StandardBuff(string displayName, string description, bool hideTime = false, bool save = false, bool persistent = false, bool canBeCleared = true)
         {
-        }
-
-        protected StandardBuff(Dictionary<GameCulture, string> displayNames, Dictionary<GameCulture, string> descriptions, bool hideTime = false, bool save = false, bool persistent = false, bool canBeCleared = true)
-        {
-            DisplayNames = displayNames;
-            Descriptions = descriptions;
+            DefaultDisplayName = displayName;
+            DefaultDescription = description;
 
             HideTime = hideTime;
             Save = save;
             Persistent = persistent;
-
             CanBeCleared = canBeCleared;
-
-            this.Register();
-        }
-
-        protected StandardBuff((GameCulture culture, string displayName, string description) str, bool hideTime = false, bool save = false, bool persistent = false, bool canBeCleared = true)
-            : this(new[] { str }, hideTime, save, persistent, canBeCleared)
-        {
-        }
-
-        protected StandardBuff((GameCulture culture, string displayName, string description)[] strings, bool hideTime = false, bool save = false, bool persistent = false, bool canBeCleared = true)
-            : this(new Dictionary<GameCulture, string>(), new Dictionary<GameCulture, string>(), hideTime, save, persistent, canBeCleared)
-        {
-            foreach (var str in strings)
-            {
-                DisplayNames.Add(str.culture, str.displayName);
-                Descriptions.Add(str.culture, str.description);
-            }
         }
 
 
@@ -56,14 +24,10 @@ namespace WebmilioCommons.Buffs
         {
             base.SetStaticDefaults();
 
-            DisplayName.SetDefault(DisplayNames.First().Value);
-            Description.SetDefault(Descriptions.First().Value);
+            DisplayName.SetDefault(DefaultDisplayName);
 
-            foreach (KeyValuePair<GameCulture, string> displayName in DisplayNames)
-                DisplayName.AddTranslation(displayName.Key, displayName.Value);
-
-            foreach (KeyValuePair<GameCulture, string> tooltip in Descriptions)
-                Description.AddTranslation(tooltip.Key, tooltip.Value);
+            if (!string.IsNullOrWhiteSpace(DefaultDescription))
+                Description.SetDefault(DefaultDescription);
 
             Main.buffNoTimeDisplay[Type] = HideTime;
             Main.buffNoSave[Type] = !Save;
@@ -71,8 +35,8 @@ namespace WebmilioCommons.Buffs
         }
 
 
-        protected Dictionary<GameCulture, string> DisplayNames { get; }
-        protected Dictionary<GameCulture, string> Descriptions { get; }
+        protected string DefaultDisplayName { get; }
+        protected string DefaultDescription { get; }
 
         public bool HideTime { get; }
         public bool Save { get; }
