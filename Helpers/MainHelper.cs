b06facfcffcb214d1d253874e5d6d93248045c0a
webmilio <webmilio@users.noreply.github.com>
2021-11-26 17:19:00 -0500

    Added MainHelper.

---
+++ Helpers/MainHelper.cs
@@ -0,0 +1,44 @@
+﻿using Terraria;
+
+namespace WebmilioCommons.Helpers;
+
+public static class MainHelper
+{
+    public const int Begin = 0,
+        NightDuration = 32_400,
+        DayDuration = 54_000,
+
+        NightBegin = 0,
+        NightEnd = (NightDuration / 2),
+
+        DayBegin = NightEnd + 1,
+        DayEnd = DayBegin + DayDuration,
+        
+        CompleteEnd = DayEnd + NightEnd;
+
+    public static double ZeroedTime
+    {
+        get
+        {
+            var time = Main.time;
+
+            if (Main.dayTime)
+            {
+                time += DayBegin;
+            }
+            else
+            {
+                if (time > NightEnd)
+                {
+                    time -= NightEnd;
+                }
+                else
+                {
+                    time += DayEnd;
+                }
+            }
+
+            return time;
+        }
+    }
+}
