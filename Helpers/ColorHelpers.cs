fa74fceb8c695d1ef946cc27d2a707dca78cd201
webmilio <webmilio@users.noreply.github.com>
2022-02-25 00:59:14 -0500

    Added color addition/substraction methods.

---
+++ Helpers/ColorHelpers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace WebmilioCommons.Helpers;
+
+public static class ColorHelpers
+{
+    public static void Add(ref Color a, Color b)
+    {
+        a.R += b.R;
+        a.G += b.G;
+        a.B += b.B;
+        a.A += b.A;
+    }
+
+    public static void Substract(ref Color a, Color b)
+    {
+        a.R -= b.R;
+        a.G -= b.G;
+        a.B -= b.B;
+        a.A -= b.A;
+    }
+}
