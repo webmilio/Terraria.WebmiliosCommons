1b515feaa005b82f394793db6d482d741acbeeb9
webmilio <webmilio@users.noreply.github.com>
2022-02-25 02:29:15 -0500

    Removed old ModLinked interface.

--- Commons/IModLinked.cs
+++ Commons/IModLinked.cs
@@ -1,8 +1,10 @@
 ﻿using Terraria.ModLoader;
 
-namespace WebmilioCommons.Commons;
-
-public interface IModLinked
+namespace WebmilioCommons.Commons
 {
-    public Mod Mod { get; set; }
+    /// <summary>Implementing this interface will cause the loader to automatically feed the owner <see cref="Terraria.ModLoader.Mod"/> to the requested instance when using <see cref="Loader{T}.New(ushort)"/> or <see cref="Loader{T}.New{TType}"/></summary>
+    public interface IModLinked
+    {
+        Mod Mod { get; set; }
+    }
 }
