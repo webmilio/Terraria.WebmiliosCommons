6680c3c0b660ee6e7f43ad24fec324d7f9fb026d
webmilio <webmilio@users.noreply.github.com>
2022-04-23 16:18:54 -0400

    Added registries

---
+++ Registries/Registry.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace WebmilioCommons.Registries;
+
+public class Registry<T>
+{
+    protected readonly List<T> items = new();
+
+    public Registry()
+    {
+        Items = items.AsReadOnly();
+    }
+
+    public virtual void Add(T item) => items.Add(item);
+    public virtual void AddRange(IEnumerable<T> source) => items.AddRange(source);
+
+    public ReadOnlyCollection<T> Items { get; }
+}
